d4493795e09910592ce6b90f5595f3beeb9c8e81
Vlad Colpman <VladislavColpman@gmail.com>
2024-07-09 14:43:38 +1200

    Added robot pos

---
+++ robot_pos/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/mechp4p/p4p/autonomous_manipulator/robot_pos/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
