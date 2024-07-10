0e326d8997343ab0385b196864ea8b1703d67401
Vlad Colpman <VladislavColpman@gmail.com>
2024-07-10 15:46:13 +1200

    Added reverse kinematics and object

--- robot_pos/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mechp4p/p4p/autonomous_manipulator/robot_pos/src
-# Build directory: /home/mechp4p/p4p/autonomous_manipulator/robot_pos/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- robot_pos/build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/mechp4p/p4p/autonomous_manipulator/robot_pos/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
